be55f80341037e7afed35391593f8c5bddbbb72d
Emilysav <70286291+Emilysav@users.noreply.github.com>
2020-11-09 22:15:43 -0500

    p

--- Assignments/Assignment 7/prime_or_composite.py
+++ Assignments/Assignment 7/prime_or_composite.py
@@ -1,15 +1,13 @@
-def prime_or_composite(number):
-    """
-    - Add code in the defined function to figure out whether or not the given number is a prime number or not. 
 
-    - A prime number (or a prime) is a natural number greater than 1 that is not a product of two smaller natural numbers. A natural number greater than 1 that is not prime is called a composite number. - Wikipedia
-    
-    - Take in a parameter called number and return “Prime” or “Composite”
-    """
+def prime_or_composite(number):
+    num1 = number - 1
+    for i in range(2,num1):
+        if number % i == 0:
+            return False
     pass
 
 if __name__ == "__main__":
-    numbers = [1, 2, 10, 31, 47, 89, 101, 103, 97, 187, 981, 19201]
+    numbers = [1, 2, 10, 31, 47, 89, 101, 103, 97, 187, 981, 19201, 47055833459]
     # If you want to test the efficency of your algorithm add this number to the array above -7
     # If you want to test the efficency of your algorithm add this number to the array above 47055833459
     answers = []
@@ -17,3 +15,4 @@ if __name__ == "__main__":
         answers.append(prime_or_composite(number))
     
     print(answers)
+    

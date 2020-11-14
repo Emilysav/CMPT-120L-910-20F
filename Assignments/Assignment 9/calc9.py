342eef01f64717385cd1a6c9e7f642140ad7631e
Emilysav <70286291+Emilysav@users.noreply.github.com>
2020-11-14 18:43:15 -0500

    9

---
+++ Assignments/Assignment 9/calc9.py
@@ -0,0 +1,6 @@
+from Calculator import Calculator
+
+print(Calculator.division(10, 5, 5))
+print(Calculator.addition(5, 6, 7))
+print(Calculator.multiplication(7, 5, 4))
+print(Calculator.exponent(2, 4, 6))
